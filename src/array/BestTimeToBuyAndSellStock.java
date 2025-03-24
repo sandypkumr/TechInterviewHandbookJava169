d6b087eb7a954f329c4dca361bcc357745601036
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-03-24 18:36:43 +0530

    Best Time To Buy And Sell Stock

---
+++ src/array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+package array;
+
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
