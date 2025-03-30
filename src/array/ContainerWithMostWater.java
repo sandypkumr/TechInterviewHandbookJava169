1bbbcfd01374dcb11b06db1afac12b50c4dc0fca
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-03-30 14:57:11 +0530

    Container With Most Water

---
+++ src/array/ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+package array;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            int width = right - left;
+            int minHeight = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, width * minHeight);
+
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
