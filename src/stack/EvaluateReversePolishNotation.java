16075e7fad43b4e73e9e6726c542b79180d178f8
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-04-02 08:31:50 +0530

    Evaluate Reverse Polish Notation

---
+++ src/stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,35 @@
+package stack;
+
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(applyOperator(a, b, token));
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private int applyOperator(int a, int b, String operator) {
+        return switch (operator) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            case "/" -> a / b;
+            default -> throw new IllegalArgumentException("Invalid operator: " + operator);
+        };
+    }
+}
