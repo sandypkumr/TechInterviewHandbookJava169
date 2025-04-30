c112b2722b6b70ce61cb248a30dfd2e6fa58a8e7
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-04-30 20:14:42 +0530

    Basic Calculator 2

---
+++ src/stack/BasicCalculator2.java
@@ -0,0 +1,32 @@
+package stack;
+
+import java.util.Stack;
+
+public class BasicCalculator2 {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        char sign = '+';
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            if (!Character.isDigit(c) && c != ' ' || i == s.length() - 1) {
+                switch (sign) {
+                    case '+' -> stack.push(num);
+                    case '-' -> stack.push(-num);
+                    case '*' -> stack.push(stack.pop() * num);
+                    case '/' -> stack.push(stack.pop() / num);
+                }
+                sign = c;
+                num = 0;
+            }
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
+    }
+}
