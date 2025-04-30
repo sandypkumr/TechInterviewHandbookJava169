7ce0841de3cef0aee71f8baf64a16ee0bac01583
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-04-30 23:24:44 +0530

    Longest Valid Parentheses

---
+++ src/stack/LongestValidParentheses.java
@@ -0,0 +1,25 @@
+package stack;
+
+import java.util.Stack;
+
+public class LongestValidParentheses {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+        return maxLength;
+    }
+}
