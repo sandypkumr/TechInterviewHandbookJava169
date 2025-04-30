b5a45bed31034e529596f39c8de8c0751cfec0fe
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-04-30 23:41:37 +0530

    Merge Two Sorted Lists

---
+++ src/linked_list/MergeTwoSortedLists.java
@@ -0,0 +1,35 @@
+package linked_list;
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode mergedHead = new ListNode(0);
+        ListNode current = mergedHead;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return mergedHead.next;
+    }
+}
+
