685b822a3829822846990f2e762ec5ab58adb479
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-05-10 22:55:51 +0530

    Subtree Of Another Tree

---
+++ src/binary_tree/SubtreeOfAnotherTree.java
@@ -0,0 +1,23 @@
+package binary_tree;
+
+public class SubtreeOfAnotherTree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
