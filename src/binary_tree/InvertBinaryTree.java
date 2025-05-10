884b306cfbd5e2e1daf41c5ab0aad8f6d57b4c97
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-05-10 19:56:14 +0530

    Invert Binary Tree

---
+++ src/binary_tree/InvertBinaryTree.java
@@ -0,0 +1,20 @@
+package binary_tree;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        // Swap the left and right children
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // Recursively invert the left and right subtrees
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
