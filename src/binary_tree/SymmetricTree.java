63ec7fdf1a86d61bea018c436ac20339ca543ed8
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-05-10 22:45:16 +0530

    Symmetric Tree

---
+++ src/binary_tree/SymmetricTree.java
@@ -0,0 +1,17 @@
+package binary_tree;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return left.val == right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
