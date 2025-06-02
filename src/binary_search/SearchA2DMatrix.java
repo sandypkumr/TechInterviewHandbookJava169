eedf444c6f6e55e1ac5807f202888370187bdb7c
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-06-02 13:49:46 +0530

    Search A 2D Matrix

---
+++ src/binary_search/SearchA2DMatrix.java
@@ -0,0 +1,26 @@
+package binary_search;
+
+public class SearchA2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / cols][mid % cols];
+
+            if (midValue == target) {
+                return true; // Target found
+            } else if (midValue < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+
+        return false; // Target not found
+    }
+}
