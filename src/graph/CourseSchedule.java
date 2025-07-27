d9850a15b45b818ae070a80d63da5bc7eed6d43b
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-07-27 19:21:34 +0530

    Course Schedule

---
+++ src/graph/CourseSchedule.java
@@ -0,0 +1,47 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CourseSchedule {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] pre : prerequisites) {
+            int course = pre[0];
+            int dependency = pre[1];
+            graph.get(dependency).add(course);
+            indegree[course]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int finishedCourses = 0;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            finishedCourses++;
+
+            for (int neighbor : graph.get(current)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        return finishedCourses == numCourses;
+    }
+}
