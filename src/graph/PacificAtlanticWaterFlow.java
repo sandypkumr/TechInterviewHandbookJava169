9669f9c1c359fcb764883dbb0bf762038aaf52a7
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-08-19 20:17:04 +0530

    Pacific Atlantic Water Flow

---
+++ src/graph/PacificAtlanticWaterFlow.java
@@ -0,0 +1,50 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PacificAtlanticWaterFlow {
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        int rows = heights.length;
+        int cols = heights[0].length;
+
+        boolean[][] pacific = new boolean[rows][cols];
+        boolean[][] atlantic = new boolean[rows][cols];
+
+        for (int c = 0; c < cols; c++) {
+            dfs(heights, 0, c, pacific, heights[0][c]);
+            dfs(heights, rows - 1, c, atlantic, heights[rows - 1][c]);
+        }
+        for (int r = 0; r < rows; r++) {
+            dfs(heights, r, 0, pacific, heights[r][0]);
+            dfs(heights, r, cols - 1, atlantic, heights[r][cols - 1]);
+        }
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (pacific[r][c] && atlantic[r][c]) {
+                    result.add(Arrays.asList(r, c));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(int[][] heights, int r, int c, boolean[][] visited, int prevHeight) {
+        int rows = heights.length;
+        int cols = heights[0].length;
+
+        if (r < 0 || r >= rows || c < 0 || c >= cols || visited[r][c] || heights[r][c] < prevHeight) {
+            return;
+        }
+        visited[r][c] = true;
+        dfs(heights, r + 1, c, visited, heights[r][c]);
+        dfs(heights, r - 1, c, visited, heights[r][c]);
+        dfs(heights, r, c + 1, visited, heights[r][c]);
+        dfs(heights, r, c - 1, visited, heights[r][c]);
+    }
+}
