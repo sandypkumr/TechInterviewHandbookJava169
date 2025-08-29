c0fcb0b6b6b3620e2a5ec56cc7927810236947fb
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-08-29 11:01:33 +0530

    Minimum Knight Moves

--- src/graph/MinimumKnightMoves.java
+++ src/graph/MinimumKnightMoves.java
@@ -5,6 +5,27 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
+/**
+ * You are given a chessboard with an infinite number of squares in all directions. A knight is placed on the square
+ * (0, 0). The knight can move to any of the squares that are two squares away horizontally and one square vertically,
+ * or two squares vertically and one square horizontally. This means that from the square (x, y), the knight can move to
+ * any of the following eight squares:
+ *
+ * (x + 2, y + 1), (x + 2, y - 1), (x - 2, y + 1), (x - 2, y - 1),
+ * (x + 1, y + 2), (x + 1, y - 2), (x - 1, y + 2), (x - 1, y - 2).
+ *
+ * Given two integers x and y, return the minimum number of moves required for the knight to reach the square (x, y).
+ *
+ * Example 1:
+ * Input: x = 2, y = 1
+ * Output: 1
+ * Explanation: The knight can reach the target square in one move: (0, 0) -> (2, 1).
+ *
+ * Example 2:
+ * Input: x = 5, y = 5
+ * Output: 4
+ * Explanation: The knight can reach the target square in four moves: (0, 0) -> (2, 1) -> (4, 2) -> (3, 4) -> (5, 5).
+ */
 public class MinimumKnightMoves {
     public int minKnightMoves(int x, int y) {
         x = Math.abs(x);
