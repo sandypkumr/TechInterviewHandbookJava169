f93f5b44d4d5bd32769c694224558eda2d31f2a9
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-08-31 16:55:38 +0530

    Inorder Successor In BST

---
+++ src/binary_search_tree/InorderSuccessorInBST.java
@@ -0,0 +1,16 @@
+package binary_search_tree;
+
+public class InorderSuccessorInBST {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode successor = null;
+        while (root != null) {
+            if (p.val < root.val) {
+                successor = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return successor;
+    }
+}
