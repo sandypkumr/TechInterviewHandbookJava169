1307a92af0ad02943853133fada6a7d4ff3762fd
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-08-31 16:12:40 +0530

    Convert Sorted Array To Binary Search Tree

---
+++ src/binary_search_tree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,18 @@
+package binary_search_tree;
+
+public class ConvertSortedArrayToBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = buildBST(nums, left, mid - 1);
+        node.right = buildBST(nums, mid + 1, right);
+        return node;
+    }
+}
