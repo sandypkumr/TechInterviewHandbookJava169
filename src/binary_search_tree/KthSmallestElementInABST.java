b2e4ee146de2b3335c9e9d152c69c7fdef060358
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-08-31 16:37:16 +0530

    Kth Smallest Element In A BST

---
+++ src/binary_search_tree/KthSmallestElementInABST.java
@@ -0,0 +1,24 @@
+package binary_search_tree;
+
+public class KthSmallestElementInABST {
+    int count = 0;
+    int result = -1;
+
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return result;
+    }
+
+    private void inorder(TreeNode node, int k) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, k);
+        count++;
+        if (count == k) {
+            result = node.val;
+            return;
+        }
+        inorder(node.right, k);
+    }
+}
