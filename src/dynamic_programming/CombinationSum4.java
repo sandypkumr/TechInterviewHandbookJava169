57529209e9baf1b856ad734b9cd7bc18051273e5
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-09-02 13:46:44 +0530

    Combination Sum 4

---
+++ src/dynamic_programming/CombinationSum4.java
@@ -0,0 +1,18 @@
+package dynamic_programming;
+
+public class CombinationSum4 {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (i - num >= 0) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+
+        return dp[target];
+    }
+}
