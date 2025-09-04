bba0099f7486e5622ea39c8ef2bee9feef104145
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-09-04 19:01:54 +0530

    Missing Number

---
+++ src/binary/MissingNumber.java
@@ -0,0 +1,13 @@
+package binary;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+        return expectedSum - actualSum;
+    }
+}
