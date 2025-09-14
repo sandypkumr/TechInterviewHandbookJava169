5ed71c804fe0b29b266ea8bc82056189091bb0de
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-09-14 19:38:00 +0530

    Permutations

---
+++ src/recursion/Permutations.java
@@ -0,0 +1,30 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums, new boolean[nums.length]);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, boolean[] used) {
+        if (temp.size() == nums.length) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            temp.add(nums[i]);
+
+            backtrack(result, temp, nums, used);
+
+            used[i] = false;
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
