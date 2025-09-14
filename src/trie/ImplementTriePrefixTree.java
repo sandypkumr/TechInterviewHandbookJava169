9ad425fcbcb59be1eb871773862fe2cb735a601c
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-09-14 17:54:07 +0530

    Implement Trie Prefix Tree

---
+++ src/trie/ImplementTriePrefixTree.java
@@ -0,0 +1,55 @@
+package trie;
+
+public class ImplementTriePrefixTree {
+    class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            isEndOfWord = false;
+        }
+    }
+
+    private final TrieNode root;
+
+    public ImplementTriePrefixTree() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node.isEndOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char ch : prefix.toCharArray()) {
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return true;
+    }
+}
